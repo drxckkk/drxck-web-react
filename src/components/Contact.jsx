71c973ebb6814efb03f5fa96b4ad462d30a2d2a4
drxckkk <drxckpro123@gmail.com>
2025-07-11 17:57:46 -0300

    Fixed text decoration for contacts, page loading.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,11 +10,11 @@ function Contact() {
                 <h2>Contact</h2>
                 <p className="email-text">
                     <i className="fas fa-envelope"></i>
-                    <a href="mailto:drxckpro123@gmail.com">drxckpro123@gmail.com</a>
+                    <a className="contact-text" href="mailto:drxckpro123@gmail.com">drxckpro123@gmail.com</a>
                     <CIcon icon={cibDiscord} />
-                    <a href="https://discordapp.com/users/drxck" target="_blank" rel="noopener noreferrer">drxck</a>
+                    <a className="contact-text">drxck</a>
                 </p>
-                <p className="payment-text">When contacting me for business, you agree with my <a href="https://docs.google.com/document/d/1lAAyg9JxJ-Uc4r9aaH-KvpafnYdOsE_U">Terms of Service</a> and agree on using <CIcon icon={cibPaypal} /> PayPal for transactions.</p>
+                <p className="payment-text">When contacting me for business, you agree with my <a className="terms-text" href="https://docs.google.com/document/d/1lAAyg9JxJ-Uc4r9aaH-KvpafnYdOsE_U">Terms of Service</a> and agree on using <CIcon icon={cibPaypal} /> PayPal for transactions.</p>
             </div>
         </section>
     );
