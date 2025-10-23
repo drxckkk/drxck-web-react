298ca14c46d11c7d3a40f50473e7af0592846bec
drxckkk <drxckpro123@gmail.com>
2025-10-23 15:45:33 -0300

    Removed kofi and improved header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,28 +4,36 @@ import { cibKoFi } from "@coreui/icons";
 
 function Header() {
   return (
-    <header>
-      <div className="container">
-        <h1>Drxck's Portfolio</h1>
-        <nav>
-          <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="#projects">Projects</a></li>
-            <li><a href="#contact">Contact</a></li>
-          </ul>
-        </nav>
-        <div className="kofi-container">
-          <a
-            href="https://ko-fi.com/drxck"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="kofi-button"
-          >
-            <span className="kofi-text"><CIcon icon={cibKoFi} /> Support me on Ko-fi</span>
-          </a>
-        </div>
+    <>
+      <div className="header-wrapper">
+        <header>
+          <div className="container">
+            <h1>Drxck's Portfolio</h1>
+            <nav>
+              <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#contact">Contact</a></li>
+              </ul>
+            </nav>
+          </div>
+        </header>
       </div>
-    </header>
+      <svg xmlns="http://www.w3.org/2000/svg" version="1.1" style={{ display: 'none' }}>
+        <defs>
+          <filter id="displacementFilter">
+            <feTurbulence type="turbulence" 
+                baseFrequency="0.01" 
+                numOctaves="2" 
+                result="turbulence" />
+    
+            <feDisplacementMap in="SourceGraphic"
+                in2="turbulence"    
+                            scale="200" xChannelSelector="R" yChannelSelector="G" />
+        </filter>
+        </defs>
+      </svg>
+    </>
   );
 }
 
