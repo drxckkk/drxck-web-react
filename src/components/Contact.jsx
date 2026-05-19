475a791cf2420bef3bcac743fc832c4c0ee54b74
drxckkk <sngking96@gmail.com>
2026-05-19 11:51:57 -0300

    Trying to fix domain issues

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,7 +5,7 @@ function Contact() {
         <section id="contact" className="contacts-container">
             <div className="contact-container">
                 <h2 className="title">Interested? Let me know.</h2>
-                <p>Either email me as I can be recognized on the mail 'drxckpro123@gmail.com'</p>
+                <p>Either email me at 'drxckpro123@gmail.com'</p>
                 <p>or don't mind easily messaging me through Discord as 'drxck'</p>
                 <p>When contacting me for business, you agree with my <a className="terms-text" href="https://docs.google.com/document/d/1lAAyg9JxJ-Uc4r9aaH-KvpafnYdOsE_U">Terms of Service</a> and agree on using PayPal for transactions.</p>
             </div>
